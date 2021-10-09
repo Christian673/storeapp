c641c251907317cdf6747233603ce5c6505c0b77
Christian673 <67290704+Christian673@users.noreply.github.com>
2021-10-09 18:24:35 -0400

    first commit

---
+++ js-final-boilerplates/client-react/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import  Task from "./components/Task";
+import "./App.css";
+import authProvider from "./authProvider";
+
+function App() {
+  return (
+    <div className="App">
+      <Task />
+    </div>
+  );
+}
+
+
+export default App;
